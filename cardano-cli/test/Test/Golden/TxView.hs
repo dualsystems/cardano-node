e585d2937c0cbfe003c9bef2f55a4294ec215331
Yuriy Syrovetskiy <yuriy.syrovetskiy@iohk.io>
2021-03-15 20:14:18 +0300

    Add another golden test

--- cardano-cli/test/Test/Golden/TxView.hs
+++ cardano-cli/test/Test/Golden/TxView.hs
@@ -27,9 +27,13 @@ golden_view_shelley =
         [ "transaction", "build-raw"
         , "--shelley-era"
         , "--tx-in"
-        ,   "fe5dd07fb576bff960d6e066eade5b26cdb5afebe29f76ea58d0a098bce5d891#29"
+        ,   "fe5dd07fb576bff960d6e066eade5b26cdb5afebe29f76ea58d0a098bce5d891\
+            \#29"
         , "--tx-out"
         ,   "addr1v9wmu83pzajplrtpsq6tsqdgwr98x888trpmah2u0ezznsge7del3+31"
+        , "--tx-out"
+        ,   "addr_test1qrefnr4k09pvge6dq83v6s67ruter8sftmky8qrmkqqsxy7q5psgn8tgqmupq4r79jmxlyk4eqt6z6hj5g8jd8393msqaw47f4\
+            \+34"
         , "--fee", "32"
         , "--invalid-hereafter", "33"
         , "--out-file", transactionBodyFile
@@ -56,6 +60,14 @@ golden_view_shelley =
       , "    network: Mainnet"
       , "    stake reference: StakeRefNull"
       , "  amount: 31"
+      , "- address:"
+      , "    Bech32: addr_test1qrefnr4k09pvge6dq83v6s67ruter8sftmky8qrmkqqsxy7q5psgn8tgqmupq4r79jmxlyk4eqt6z6hj5g8jd8393msqaw47f4"
+      , "    credential:"
+      , "      key hash: f2998eb67942c4674d01e2cd435e1f17919e095eec43807bb0010313"
+      , "    network: Testnet"
+      , "    stake reference: \
+              \StakeRefBase (KeyHashObj (KeyHash \"c0a060899d6806f810547e2cb66f92d5c817a16af2a20f269e258ee0\"))"
+      , "  amount: 34"
       , "time to live: 33"
       , "update: null"
       , "withdrawals: []"
